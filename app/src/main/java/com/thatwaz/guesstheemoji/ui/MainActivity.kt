ce7fde7a4cdab10e26cab50881a02c2336e031a9
thatwaz <wazowski0425@gmail.com>
2025-10-05 05:35:15 -0400

    set orientation to portrait

--- app/src/main/java/com/thatwaz/guesstheemoji/ui/MainActivity.kt
+++ app/src/main/java/com/thatwaz/guesstheemoji/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.thatwaz.guesstheemoji.ui
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,10 +24,11 @@ import com.thatwaz.guesstheemoji.ui.theme.GuessTheEmojiTheme
 
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val prefs = Prefs(this)
-
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContent {
             GuessTheEmojiTheme {
                 val nav = rememberNavController()
